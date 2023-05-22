c25b83938ab9aedbb342b69a089f506398850d4e
ocruzmartz <00109321@uca.edu.sv>
2023-05-22 13:38:06 -0600

    Fix repository file

--- TiendaTracker/app/src/main/java/com/ocruzmartz/tiendatracker/repositories/TiendaRepository.kt
+++ TiendaTracker/app/src/main/java/com/ocruzmartz/tiendatracker/repositories/TiendaRepository.kt
@@ -3,7 +3,7 @@ package com.ocruzmartz.tiendatracker.repositories
 import com.ocruzmartz.tiendatracker.data.model.TiendaModel
 import com.ocruzmartz.tiendatracker.data.tienda
 
-class TiendaRepository {
-    fun getTienda() = tienda
-    fun addTiendas(car: TiendaModel) = tienda.add(car)
+class TiendaRepository(private val tiendas: MutableList<TiendaModel>) {
+    fun getTienda() = tiendas
+    fun addTiendas(tienda: TiendaModel) = tiendas.add(tienda)
 }
